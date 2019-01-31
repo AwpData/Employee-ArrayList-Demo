8be8f88109d306ff6a9f0bfe9c10d7faff3cb217
AwpData <awpdata@gmail.com>
2019-01-30 18:36:30 -0800

    Update Buisness.java

--- Buisness.java
+++ Buisness.java
@@ -1,21 +1,19 @@
-public class Buisness{ 
-    private String id; 
-    private String name; 
-    private String DOB; 
-    private String SkillSetName; 
-    private String ProjectName; 
-     
-    public Buisness(String id, String name, String DOB, String SkillSetName, String ProjectName) { 
-        this.id = id; 
-        this.name = name; 
-        this.DOB = DOB; 
-        this.SkillSetName = SkillSetName; 
-        this.ProjectName = ProjectName; 
-    } 
-     
-     
-    public String toString() { //test method
-        return id + " " + name + " " + DOB + " " + SkillSetName + " " + ProjectName; 
-    } 
-} 
- 
+public class Buisness {
+	private String id;
+	private String name;
+	private String DOB;
+	private String SkillSetName;
+	private String ProjectName;
+
+	public Buisness(String id, String name, String DOB, String SkillSetName, String ProjectName) {
+		this.id = id;
+		this.name = name;
+		this.DOB = DOB;
+		this.SkillSetName = SkillSetName;
+		this.ProjectName = ProjectName;
+	}
+
+	public String toString() { // test method
+		return id + " " + name + " " + DOB + " " + SkillSetName + " " + ProjectName;
+	}
+}
